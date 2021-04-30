373a42579c547628d35d63e787b6ea9c7bbb91c5
xecua <courser4848@gmail.com>
2021-04-30 21:20:48 +0900

    きれいになった

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.kt
@@ -4,23 +4,40 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import jp.ac.titech.itpro.sdl.camera.MainActivity
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private var photoImage: Bitmap? = null
+    private var currentPhotoUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val photoButton = findViewById<Button>(R.id.photo_button)
         photoButton.setOnClickListener { v: View? ->
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val distFile = try {
+                createTempFile()
+            } catch (e: Exception) {
+                return@setOnClickListener
+            }
+            currentPhotoUri = FileProvider.getUriForFile(this, "${BuildConfig.APPLICATION_ID}.provider", distFile)
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
+                it.putExtra(MediaStore.EXTRA_OUTPUT, currentPhotoUri)
+                it.flags = Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+            }
             val manager = packageManager
             @SuppressLint("QueryPermissionsNeeded") val activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
             if (activities.isNotEmpty()) {
@@ -31,21 +48,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @Throws(IOException::class)
+    private fun createTempFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        return File.createTempFile("temp_${timeStamp}", ".png", cacheDir)
+    }
+
     private fun showPhoto() {
-        if (photoImage == null) {
+        if (currentPhotoUri == null) {
             return
         }
         val photoView = findViewById<ImageView>(R.id.photo_view)
-        photoView.setImageBitmap(photoImage)
-    }
-
-    override fun onActivityResult(reqCode: Int, resCode: Int, data: Intent?) {
-        super.onActivityResult(reqCode, resCode, data)
-        if (reqCode == REQ_PHOTO) {
-            if (resCode == RESULT_OK) {
-                photoImage = data?.extras?.get("data") as Bitmap
-            }
-        }
+        photoView.setImageURI(currentPhotoUri)
     }
 
     override fun onResume() {
