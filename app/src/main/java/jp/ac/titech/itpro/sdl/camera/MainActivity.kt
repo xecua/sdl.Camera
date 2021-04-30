8af36da4781721e2eddf2db336bc8df79511c530
xecua <courser4848@gmail.com>
2021-04-30 18:04:38 +0900

    baseline

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -13,14 +14,13 @@ import androidx.appcompat.app.AppCompatActivity
 import jp.ac.titech.itpro.sdl.camera.MainActivity
 
 class MainActivity : AppCompatActivity() {
-    private val photoImage: Bitmap? = null
+    private var photoImage: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val photoButton = findViewById<Button>(R.id.photo_button)
         photoButton.setOnClickListener { v: View? ->
-            val intent = Intent()
-            // TODO: You should setup appropriate parameters for the intent
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             val manager = packageManager
             @SuppressLint("QueryPermissionsNeeded") val activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
             if (activities.isNotEmpty()) {
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(reqCode, resCode, data)
         if (reqCode == REQ_PHOTO) {
             if (resCode == RESULT_OK) {
-                // TODO: You should implement the code that retrieve a bitmap image
+                photoImage = data?.extras?.get("data") as Bitmap
             }
         }
     }
